676091d0a26c14600d8676157248142556182cb8
NovaShen555 <admin@novashen.top>
2023-04-07 22:59:11 +0800

    yaml学习

--- src/main/java/top/novashen/helloworld/pojo/Person.java
+++ src/main/java/top/novashen/helloworld/pojo/Person.java
@@ -3,12 +3,14 @@ package top.novashen.helloworld.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ConfigurationProperties(prefix = "person")
 public class Person {
     private String name;
     private Integer age;
--- src/test/java/top/novashen/helloworld/HelloworldApplicationTests.java
+++ src/test/java/top/novashen/helloworld/HelloworldApplicationTests.java
@@ -1,13 +1,19 @@
 package top.novashen.helloworld;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.novashen.helloworld.pojo.Person;
 
 @SpringBootTest
 class HelloworldApplicationTests {
 
+	@Autowired
+	private Person person;
+
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
